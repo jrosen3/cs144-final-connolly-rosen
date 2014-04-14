7d45a17f07655ddb384d4cc61b3d9bc6a5f07b49
Jared Rosen <jaredarosen@gmail.com>
2014-04-14 15:40:44 -0400

    soundave

--- vibox/vibox.ino
+++ vibox/vibox.ino
@@ -1,8 +1,8 @@
 #include <Wire.h>
 
-#define DELAY 100
+#define DELAY 0
 #define MOTION_PENALTY 1000
-#define SOUND_PENALTY 500
+#define SOUND_PENALTY 10
 #define TEMP_PENALTY 250
 
 int motionPin = 2;
@@ -10,7 +10,7 @@ int soundPin = A0;
 int tempPin = 0x48; // pin A4 (SDA) and A5 (SCL)
 
 double motionAve = 1; // 1 is empty room
-double soundAve = 9.872; // 9.872 is quite room
+double soundAve = 0; // 9.872 is quite room
 double tempAve = 25; // 25 celcious is room temp
 
 int timeNow = 0;
@@ -33,12 +33,17 @@ void loop() {
   if ((millis() - timeNow) >= DELAY) {
     // get vals
     int motionVal = digitalRead(motionPin);
-    double soundVal = 16.801 * log(analogRead(soundPin)) + 9.872;
-    double tempVal = getTemperature();
+//    double soundVal = 16.801 * log(analogRead(soundPin)) + 9.872;
+    double soundVal = analogRead(soundPin);
+    double tempVal = getTemperature(tempPin);
     
     // update aves
     motionAve = (motionVal + (MOTION_PENALTY * motionAve)) / (MOTION_PENALTY + 1);
-    soundAve = (soundVal + (SOUND_PENALTY * soundAve)) / (SOUND_PENALTY + 1);
+    if (soundVal > 0) {
+      soundAve = (soundVal + (SOUND_PENALTY * soundAve)) / (SOUND_PENALTY + 1);
+    } else {
+      soundAve = (0 + ((SOUND_PENALTY * 2) * soundAve)) / ((SOUND_PENALTY * 2) + 1);
+    }
     tempAve = (tempVal + (TEMP_PENALTY * tempAve)) / (TEMP_PENALTY + 1);
     
     // print
@@ -56,8 +61,8 @@ void loop() {
 
 
 
-float getTemperature(){
-  Wire.requestFrom(tempPin, 2); 
+float getTemperature(int tp){
+  Wire.requestFrom(tp, 2); 
 
   byte MSB = Wire.read();
   byte LSB = Wire.read();
